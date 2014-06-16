e8267d3ddcdb88ce190373aef8344de1a1dc8337
Richard Chen <rchen@twitter.com>
2014-06-16 11:55:50 -0700

    check pathogen and solarized before copying vim config in setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 set -e
 
+# Set up temporary working directory
+
 TMP_DIR='/tmp/config'
 
 rm -rf $TMP_DIR
+
+# Set up zsh
 
 if [ ! -n "$ZSH" ]; then
   ZSH=~/.oh-my-zsh
@@ -22,7 +26,7 @@ hash git >/dev/null 2>&1 && git clone https://github.com/richardchen331/config.g
 cd $TMP_DIR
 
 echo "\033[0;34mLooking for an existing zsh config...\033[0m"
-if [ -f ~/.zshrc ] || [ -f ~/.zshrc ]; then
+if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
   echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.zshrc.bak\033[0m";
 	mv ~/.zshrc ~/.zshrc.bak
 else
@@ -36,6 +40,23 @@ cp zshrc ~/.zshrc
 echo "\033[0;34mCopying zsh theme from repo...\033[0m"
 cp richardchen.zsh-theme $ZSH/themes
 
+# Set up vim
+
+echo "\033[0;34mChecking for vim pathogen...\033[0m"
+if [ ! -f ~/.vim/autoload/pathogen.vim ] && [ ! -h ~/.vim/autoload/pathogen.vim ]; then
+	echo "\033[0;34mvim pathogen not found, installing now\033[0m"
+  mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+echo "\033[0;34mChecking for vim solarized...\033[0m"
+if [ ! -d ~/.vim/bundle/vim-colors-solarized ]; then
+	echo "\033[0;34mvim solarized not found, installing now\033[0m"
+  cd ~/.vim/bundle
+  git clone git://github.com/altercation/vim-colors-solarized.git
+  mv vim-colors-solarized ~/.vim/bundle/
+  cd $TMP_DIR
+fi
+
 echo "\033[0;34mLooking for an existing vim config...\033[0m"
 if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
   echo "\033[0;33mFound ~/.vimrc.\033[0m \033[0;32mBacking up to ~/.vim.bak\033[0m";
@@ -43,5 +64,7 @@ if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
 fi
 echo "\033[0;34mCopying vim config from repo...\033[0m"
 cp vimrc ~/.vimrc
+
+# Clean up temporary working directory
 
 rm -rf $TMP_DIR
