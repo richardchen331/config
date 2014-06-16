3e5049c14727945a7d356de267e61288af612959
Richard Chen <rchen@twitter.com>
2014-06-15 23:24:32 -0700

    fix setup script bug

--- setup.sh
+++ setup.sh
@@ -1,18 +1,11 @@
-if [ -f ~/.bashrc ]:
-then
-	mv ~/.bashrc ~/.bashrc.bak
-fi
-cp bashrc ~/.bashrc
-source ~/.bashrc
-
-if [ -f ~/.zshrc ]:
+if [ -f ~/.zshrc ]
 then
 	mv ~/.zshrc ~/.zshrc.bak
 fi
 cp zshrc ~/.zshrc
 source ~/.zshrc
 
-if [ -f ~/.vimrc ]:
+if [ -f ~/.vimrc ]
 then
 	mv ~/.vimrc ~/.vimrc.bak
 fi
